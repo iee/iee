08aa80f1ce6d9f593417eeea11aa71f1c18ae553
gashmish <artem.cpt@gmail.com>
2011-07-26 23:28:12 +0400

    Temporary commit

--- src/org/eclipse/iee/sample/image/actions/AddImagePadActionDelegate.java
+++ src/org/eclipse/iee/sample/image/actions/AddImagePadActionDelegate.java
@@ -1,12 +1,9 @@
 package org.eclipse.iee.sample.image.actions;
 
-import org.eclipse.iee.editor.jdt.editors.ExtendedJavaEditor;
-import org.eclipse.iee.editor.sample.SampleExtendedEditor;
+import org.eclipse.iee.editor.IPadEditor;
 import org.eclipse.iee.sample.image.pad.ImagePad;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
@@ -15,29 +12,23 @@ public class AddImagePadActionDelegate implements IEditorActionDelegate {
 
 	Shell shell = null;
 	
-	ExtendedJavaEditor fExtendedJavaEditor;
-	
+	IPadEditor fPadEditor;
+
 	@Override
 	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-		
-		if (targetEditor instanceof ExtendedJavaEditor) {
-			fExtendedJavaEditor = (ExtendedJavaEditor) targetEditor;
+		if (targetEditor instanceof IPadEditor) {
+			fPadEditor = (IPadEditor) targetEditor;
         }
 	}
 
 	@Override
 	public void run(IAction action) {
-		Shell[] shells = Display.getCurrent().getShells();        
-		if (shells.length > 0) {
-            shell = shells[0];
-        }
-        
-		if (fExtendedJavaEditor == null) {
-            MessageDialog.openError(shell, "Invalid editor", "Invalid editor");
+		
+		if (fPadEditor == null) {
             return;
         }
 		
-		fExtendedJavaEditor.createPad(new ImagePad(), fExtendedJavaEditor.getCaretOffset());
+		fPadEditor.createPad(new ImagePad(), fPadEditor.getCaretOffset());
 	}
 
 	@Override
