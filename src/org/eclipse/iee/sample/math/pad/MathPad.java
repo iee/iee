cdd221bed4dcb70cda2911843fa1cbe043352df2
khavrenko <khavrenkosergey@gmail.com>
2011-07-29 13:04:18 +0400

    Some refactoring

--- src/org/eclipse/iee/sample/math/pad/MathPad.java
+++ src/org/eclipse/iee/sample/math/pad/MathPad.java
@@ -7,13 +7,14 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
 import java.awt.Color;
-
 import javax.swing.text.BadLocationException;
-
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.iee.editor.core.pad.Pad;
@@ -28,34 +29,53 @@ import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
 
 public class MathPad extends Pad {
-	
+
 	public MathPad() {
 		super();
 	}
-	
-	@Override
-	public void createPartControl(final Composite parent)
-	{
 
-		parent.setLayout(new RowLayout(SWT.HORIZONTAL));
-		
-		final Text text = new Text (parent, SWT.BORDER);
-		text.setBounds(this.getContainer().getComposite().getLocation().x, this.getContainer().getComposite().getLocation().y, 20, 10);
-		text.setText("Test");
-		
-		final Button calculate = new Button(parent, SWT.BUTTON1);
-		final Label label = new Label(parent, SWT.NONE);
-		final String imagePath = this.getContainerID() + ".jpg"; 
+	@Override
+	public void createPartControl(final Composite parent) {
+		parent.setLayout(new RowLayout());
+		GridLayout layout = new GridLayout(2, false);
+		layout.marginBottom = 3;
+		layout.marginRight = 3;
+		layout.marginLeft = 3;
+		layout.marginTop = 3;
+		Group mathPad = new Group(parent, SWT.NONE);
+		mathPad.setText("Sample math Pad");
+		mathPad.setBounds(parent.getBounds());
+		mathPad.setLayout(layout);
+		GridData groupGridData = new GridData();
+		groupGridData.horizontalAlignment = GridData.FILL;
+		groupGridData.verticalAlignment = GridData.FILL;
+		mathPad.setData(groupGridData);
+
+		final Text text = new Text(mathPad, SWT.BORDER);
+		text.setBounds(this.getContainer().getComposite().getLocation().x, this
+				.getContainer().getComposite().getLocation().y, 50, 10);
+		text.setText("");
+
+		final Button calculate = new Button(mathPad, SWT.BUTTON1);
+
+		final Label label = new Label(mathPad, SWT.NONE);
+		GridData labelGridData = new GridData();
+		labelGridData.horizontalAlignment = GridData.FILL;
+		labelGridData.grabExcessHorizontalSpace = true;
+		labelGridData.grabExcessVerticalSpace = true;
+		label.setLayoutData(labelGridData);
+
+		final String imagePath = this.getContainerID() + ".jpg";
 		calculate.setText("Calculate");
-		calculate.setToolTipText("Calculate textbox expression");
+		calculate.setToolTipText("Calculate expression");
 		calculate.addMouseListener(new MouseListener() {
-			
+
 			@Override
 			public void mouseUp(MouseEvent e) {
 
-				String output = "";				
+				String output = "";
 				String expression = text.getText();
-				
+
 				try {
 					F.initSymbols(null);
 					EvalUtilities util = new EvalUtilities();
@@ -83,10 +103,8 @@ public class MathPad extends Pad {
 				} catch (BadLocationException e1) {
 					e1.printStackTrace();
 				} catch (IOException e2) {
-					// TODO Auto-generated catch block
 					e2.printStackTrace();
 				} catch (Exception e3) {
-					// TODO Auto-generated catch block
 					e3.printStackTrace();
 				}
 				Image image = null;
@@ -95,31 +113,29 @@ public class MathPad extends Pad {
 				} catch (Exception exception) {
 
 					exception.printStackTrace();
-					
+
 				}
-								
 				label.setImage(image);
 				parent.pack();
 			}
-			
+
 			@Override
-			public void mouseDown(MouseEvent e) { }
-			
+			public void mouseDown(MouseEvent e) {
+			}
+
 			@Override
-			public void mouseDoubleClick(MouseEvent e) { }
+			public void mouseDoubleClick(MouseEvent e) {
+			}
 		});
-		
+
 	}
 
-	
 	protected MathPad(String containerID) {
 		super(containerID);
 	}
-	
-	
+
 	@Override
 	public Pad copy() {
-		// TODO Auto-generated method stub
 		return new MathPad();
 	}
 
@@ -129,4 +145,3 @@ public class MathPad extends Pad {
 	}
 
 }
-
