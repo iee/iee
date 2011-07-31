3145a429ac59924a763936f588e3882196df0966
khavrenko <khavrenkosergey@gmail.com>
2011-07-31 20:17:16 +0400

    Working on pad usability

--- src/org/eclipse/iee/sample/math/pad/MathPad.java
+++ src/org/eclipse/iee/sample/math/pad/MathPad.java
@@ -3,11 +3,19 @@ package org.eclipse.iee.sample.math.pad;
 import java.io.*;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
 import java.awt.Color;
 import javax.swing.text.BadLocationException;
+
+import org.eclipse.swt.graphics.FontMetrics;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.RowLayout;
@@ -29,97 +37,84 @@ import org.scilab.forge.jlatexmath.TeXFormula;
 
 public class MathPad extends Pad {
 
-	private final String fImagePath;
+	private String fImagePath;
 	private String fExpression;
+
+	private boolean fIsTextVisible;
 
 	public MathPad() {
 		super();
 		fImagePath = this.getContainerID() + ".jpg";
 		fExpression = "";
+		fIsTextVisible = true;
 	}
 
 	@Override
 	public void createPartControl(final Composite parent) {
 		parent.setLayout(new RowLayout());
-		GridLayout layout = new GridLayout(2, false);
+		FormLayout layout = new FormLayout();
 		layout.marginBottom = 3;
 		layout.marginRight = 3;
 		layout.marginLeft = 3;
 		layout.marginTop = 3;
 		Group mathPad = new Group(parent, SWT.NONE);
+		mathPad.setSize(100, 100);
 		mathPad.setText("Sample math Pad");
-		mathPad.setBounds(parent.getBounds());
 		mathPad.setLayout(layout);
-		GridData groupGridData = new GridData();
-		groupGridData.horizontalAlignment = GridData.FILL;
-		groupGridData.verticalAlignment = GridData.FILL;
-		mathPad.setData(groupGridData);
+		mathPad.setToolTipText("Press DoubleClick on expression for calculation");
 
-		final Text text = new Text(mathPad, SWT.BORDER);
-		text.setBounds(this.getContainer().getComposite().getLocation().x, this
-				.getContainer().getComposite().getLocation().y, 50, 10);
-		text.setText("");
-
-		final Button calculate = new Button(mathPad, SWT.BUTTON1);
+		final Text text = new Text(mathPad, SWT.BORDER | SWT.H_SCROLL);
+		text.setText(fExpression);
+		FormData textFormData = new FormData();
+		textFormData.top = new FormAttachment(10);
+		textFormData.left = new FormAttachment(10);
+		//textFormData.width = 100;
+		//textFormData.height = 30;
+		text.setLayoutData(textFormData);
+		text.setVisible(fIsTextVisible);
 
 		final Label label = new Label(mathPad, SWT.NONE);
-		GridData labelGridData = new GridData();
-		labelGridData.horizontalAlignment = GridData.FILL;
-		labelGridData.grabExcessHorizontalSpace = true;
-		labelGridData.grabExcessVerticalSpace = true;
-		label.setLayoutData(labelGridData);
+		FormData labelFormData = new FormData();
+		labelFormData.top = new FormAttachment(20);
+		labelFormData.left = new FormAttachment(10);
+		//labelFormData.width = 100;
+		//labelFormData.height = 30;
+		label.setLayoutData(labelFormData);
+		
+		if (!fImagePath.isEmpty()) {
+			File imageFile = new File(fImagePath);
 
-		calculate.setText("Calculate");
-		calculate.setToolTipText("Calculate expression");
-		calculate.addMouseListener(new MouseListener() {
-
-			@Override
-			public void mouseUp(MouseEvent e) {
-
-				String output = "";
-				fExpression = text.getText();
-
-				try {
-					F.initSymbols(null);
-					EvalUtilities util = new EvalUtilities();
-					IExpr result = null;
-					StringBufferWriter buf = new StringBufferWriter();
-
-					result = util.evaluate(fExpression);
-					OutputFormFactory.get().convert(buf, result);
-
-					output = buf.toString();
-
-					/* Convert to TeX and image */
-					EvalEngine engine = new EvalEngine();
-					TeXUtilities texUtil = new TeXUtilities(engine);
-
-					StringWriter stw = new StringWriter();
-					texUtil.toTeX(result, stw);
-					output = stw.toString();
-
-					TeXFormula formula = new TeXFormula(stw.toString());
-					/* */
-					formula.createJPEG(TeXConstants.STYLE_DISPLAY, 20,
-							fImagePath, Color.white, Color.black);
-
-				} catch (BadLocationException e1) {
-					e1.printStackTrace();
-				} catch (IOException e2) {
-					e2.printStackTrace();
-				} catch (Exception e3) {
-					e3.printStackTrace();
-				}
+			if (imageFile.exists()) {
 				Image image = null;
 				try {
 					image = new Image(parent.getDisplay(), fImagePath);
 				} catch (Exception exception) {
-
 					exception.printStackTrace();
-
 				}
 				label.setImage(image);
 				parent.pack();
+			}
+		}
+
+		label.setVisible(!fIsTextVisible);
+		
+		parent.pack();
+		
+		// Listeners
+
+		text.addModifyListener(new ModifyListener() {
+
+			@Override
+			public void modifyText(ModifyEvent e) {
+				fExpression = text.getText();
+			}
+		});
+
+		text.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseUp(MouseEvent e) {
+
 			}
 
 			@Override
@@ -128,6 +123,81 @@ public class MathPad extends Pad {
 
 			@Override
 			public void mouseDoubleClick(MouseEvent e) {
+				if (fIsTextVisible) {
+					text.setEditable(false);
+					String output = "";
+					try {
+						F.initSymbols(null);
+						EvalUtilities util = new EvalUtilities();
+						IExpr result = null;
+						StringBufferWriter buf = new StringBufferWriter();
+
+						if (fExpression.isEmpty())
+							result = util.evaluate("Empty string");
+						else
+							result = util.evaluate(fExpression);
+						OutputFormFactory.get().convert(buf, result);
+
+						output = buf.toString();
+
+						/* Convert to TeX and image */
+						EvalEngine engine = new EvalEngine();
+						TeXUtilities texUtil = new TeXUtilities(engine);
+
+						StringWriter stw = new StringWriter();
+						texUtil.toTeX(result, stw);
+						output = stw.toString();
+
+						TeXFormula formula = new TeXFormula(stw.toString());
+						/* */
+						formula.createJPEG(TeXConstants.STYLE_DISPLAY, 20,
+								fImagePath, Color.white, Color.black);
+
+					} catch (BadLocationException e1) {
+						e1.printStackTrace();
+					} catch (IOException e2) {
+						e2.printStackTrace();
+					} catch (Exception e3) {
+						e3.printStackTrace();
+					}
+					Image image = null;
+					try {
+						image = new Image(parent.getDisplay(), fImagePath);	
+					} catch (Exception exception) {
+
+						exception.printStackTrace();
+
+					}
+					
+					fIsTextVisible = false;
+					text.setVisible(fIsTextVisible);
+					label.setVisible(!fIsTextVisible);
+					label.setImage(image);
+					parent.pack();
+				}
+			}
+		});
+
+		label.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseUp(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseDown(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				if (!fIsTextVisible) {
+					fIsTextVisible = true;
+					text.setVisible(fIsTextVisible);
+					label.setVisible(!fIsTextVisible);
+					text.setEditable(true);
+					// mathPad.pack();
+					parent.pack();
+				}
 			}
 		});
 
@@ -136,26 +206,32 @@ public class MathPad extends Pad {
 	protected MathPad(String containerID) {
 		super(containerID);
 		fImagePath = containerID + ".jpg";
+		fExpression = "";
+		fIsTextVisible = true;
 	}
 
 	@Override
 	public Pad copy() {
 		MathPad newPad = new MathPad();
 		newPad.fExpression = this.fExpression;
+		newPad.fIsTextVisible = this.fIsTextVisible;
 		File inputFile = new File(this.fImagePath);
 		File outputFile = new File(newPad.fImagePath);
-		FileReader in;
-		FileWriter out;
-		try {
-			in = new FileReader(inputFile);
-			out = new FileWriter(outputFile);
-			int c;
 
-			while ((c = in.read()) != -1)
-				out.write(c);
-			
-			in.close();
-			out.close();
+		try {
+			BufferedInputStream bis = new BufferedInputStream(
+					new FileInputStream(inputFile), 4096);
+			BufferedOutputStream bos = new BufferedOutputStream(
+					new FileOutputStream(outputFile), 4096);
+			int buffer;
+			while ((buffer = bis.read()) != -1) {
+				bos.write(buffer);
+			}
+			bos.close();
+			bis.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
