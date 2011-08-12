27f8b0a06df508b4b5cbdaeb0179bc2769eb57cd
gashmish <artem.cpt@gmail.com>
2011-08-12 12:02:10 +0400

    some changes

---
+++ src/org/eclipse/iee/translator/math/MathCompiler.java
@@ -0,0 +1,8 @@
+package org.eclipse.iee.translator.math;
+
+public class MathCompiler {
+	
+	public static void generate(String expression) {
+		
+	}
+}
--- src/org/eclipse/iee/translator/math/pad/CompiledMathPad.java
+++ src/org/eclipse/iee/translator/math/pad/CompiledMathPad.java
@@ -3,10 +3,14 @@ package org.eclipse.iee.translator.math.pad;
 import java.io.*;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.iee.editor.core.pad.Pad;
 import org.eclipse.iee.translator.math.FileStorage;
+import org.eclipse.iee.translator.math.MathCompiler;
 
 public class CompiledMathPad extends Pad implements Serializable {
 
@@ -16,8 +20,6 @@ public class CompiledMathPad extends Pad implements Serializable {
 
 	public CompiledMathPad() {
 		super();
-		fText = "";
-		save();
 	}
 
 	@Override
@@ -28,13 +30,28 @@ public class CompiledMathPad extends Pad implements Serializable {
 		parent.setLayout(layout);
 		StyledText styledText = new StyledText(parent, SWT.NONE);
 		styledText.setText(fText);
+		
+		Button button = new Button(parent, SWT.PUSH);
+		button.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				/* Switch to welcome state */
+				getContainer();
+			}
 
+			@Override
+			public void mouseDoubleClick(MouseEvent arg0) {
+			}
+
+			@Override
+			public void mouseUp(MouseEvent arg0) {
+			}
+		});
+		
 	}
 
 	protected CompiledMathPad(String containerID) {
 		super(containerID);
-		fText = "";
-		save();
 	}
 
 	public static void setStorage(FileStorage fStorage) {
