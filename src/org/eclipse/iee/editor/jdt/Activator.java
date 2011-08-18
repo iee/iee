f5e3a641010f3094489099fac14c398176180efc
khavrenko <khavrenkosergey@gmail.com>
2011-08-19 02:02:07 +0400

    Working on #1

--- src/org/eclipse/iee/editor/jdt/Activator.java
+++ src/org/eclipse/iee/editor/jdt/Activator.java
@@ -1,5 +1,6 @@
 package org.eclipse.iee.editor.jdt;
 
+import org.eclipse.iee.editor.core.pad.PadManager;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -14,16 +15,22 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
+	private PadManager fPadManager;
+
 	/**
 	 * The constructor
 	 */
 	public Activator() {
+		fPadManager = new PadManager();
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
@@ -32,7 +39,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -41,7 +51,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
@@ -49,13 +59,19 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
+	public PadManager getPadManager() {
+		System.out.println("getPadManager");
+		return fPadManager;
 	}
 }
--- src/org/eclipse/iee/editor/jdt/editors/ExtendedJavaEditor.java
+++ src/org/eclipse/iee/editor/jdt/editors/ExtendedJavaEditor.java
@@ -1,18 +1,25 @@
 package org.eclipse.iee.editor.jdt.editors;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.iee.editor.IPadEditor;
 import org.eclipse.iee.editor.IeeEditorPlugin;
 import org.eclipse.iee.editor.core.container.ContainerManager;
+import org.eclipse.iee.editor.core.container.ContainerManagerEvent;
+import org.eclipse.iee.editor.core.container.IContainerManagerListener;
 import org.eclipse.iee.editor.core.pad.Pad;
 import org.eclipse.iee.editor.core.pad.PadManager;
 import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.swt.custom.StyledText;
 
 @SuppressWarnings("restriction")
-public class ExtendedJavaEditor extends CompilationUnitEditor {
+public class ExtendedJavaEditor extends CompilationUnitEditor implements IPadEditor{
 	
 	private ContainerManager fContainerManager;
-	private PadManager fPadManager;
+	private IContainerManagerListener fContainerManagerListener;
+	
+	private final PadManager fPadManager = IeeEditorPlugin.getDefault().getPadManager();
 	
 	public void createPartControl(org.eclipse.swt.widgets.Composite parent) {
 		super.createPartControl(parent);
@@ -20,20 +27,44 @@ public class ExtendedJavaEditor extends CompilationUnitEditor {
 		initIeeEditorCore();
 	};
 	
-	protected void initIeeEditorCore() {
+	public void initIeeEditorCore() {
 		StyledText styledText = getSourceViewer().getTextWidget();
 		IDocument document = getSourceViewer().getDocument();
 
 		fContainerManager = new ContainerManager(document, styledText);
 		
-		fPadManager = IeeEditorPlugin.getDefault().getPadManager();
+		fContainerManagerListener = new IContainerManagerListener() {
+			@Override
+			public void debugNotification(ContainerManagerEvent event) {
+				firePropertyChange(PROP_CONTAINER_SET);
+			}
+			@Override public void containerCreated(ContainerManagerEvent event) {}
+			@Override public void containerRemoved(ContainerManagerEvent event) {}			
+		};
+		fContainerManager.addContainerManagerListener(fContainerManagerListener);
+		
 		fPadManager.registerContainerManager(fContainerManager);
+		
+		
+		/* Update document partitioning.
+		 * 
+		 * TODO: find smarter way. 
+		 */		
+		try {
+			String text = document.get();
+			document.replace(0, text.length(), "");
+			document.set(text);
+		} catch (BadLocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void dispose() {
 		fPadManager.removeContainerManager(fContainerManager);
+		fContainerManager.removeContainerManagerListener(fContainerManagerListener);
 		fContainerManager = null;
-				
+		
 		super.dispose();
 	}
 	
@@ -44,4 +75,20 @@ public class ExtendedJavaEditor extends CompilationUnitEditor {
 	public int getCaretOffset() {
 		return getSourceViewer().getTextWidget().getCaretOffset();
 	}
+
+	@Override
+	public Object[] getElements() {
+		return fContainerManager.getElements();
+	}
+	
+	public void doSave(IProgressMonitor monitor) {
+		fPadManager.savePadsInEditor(fContainerManager.getContainerManagerID());
+		super.doSave(monitor);
+	}
+	
+	public void doSaveAs() {
+		fPadManager.savePadsInEditor(fContainerManager.getContainerManagerID());
+		super.doSaveAs();
+	}
+	
 }
