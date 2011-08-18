07b6532e98363aee7cd0ad0fdcfb7fbc8e0502f5
khavrenko <khavrenkosergey@gmail.com>
2011-08-19 03:05:45 +0400

    Refactoring. Add ExtendedJavaEditor support.

--- src/org/eclipse/iee/sample/image/actions/AddImagePadActionDelegate.java
+++ src/org/eclipse/iee/sample/image/actions/AddImagePadActionDelegate.java
@@ -3,6 +3,7 @@ package org.eclipse.iee.sample.image.actions;
 import org.eclipse.iee.editor.IPadEditor;
 import org.eclipse.iee.sample.image.pad.ImagePad;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorActionDelegate;
@@ -11,23 +12,26 @@ import org.eclipse.ui.IEditorPart;
 public class AddImagePadActionDelegate implements IEditorActionDelegate {
 
 	Shell shell = null;
-	
+
 	IPadEditor fPadEditor;
 
 	@Override
 	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-		if (targetEditor instanceof IPadEditor) {
+		try {
 			fPadEditor = (IPadEditor) targetEditor;
-        }
+		} catch (ClassCastException e) {
+			fPadEditor = null;
+		}
 	}
 
 	@Override
 	public void run(IAction action) {
-		
+
 		if (fPadEditor == null) {
-            return;
-        }
-		
+			MessageDialog.openError(shell, "Invalid editor", "Invalid editor");
+			return;
+		}
+
 		fPadEditor.createPad(new ImagePad(), fPadEditor.getCaretOffset());
 	}
 
