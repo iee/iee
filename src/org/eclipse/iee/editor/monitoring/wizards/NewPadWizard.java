bbb17e39f80865c5712d7ec250dc5da07d50ece7
khavrenko <khavrenkosergey@gmail.com>
2011-08-30 18:54:06 +0400

    Starting implementing NewPad wizard

---
+++ src/org/eclipse/iee/editor/monitoring/wizards/NewPadWizard.java
@@ -0,0 +1,26 @@
+package org.eclipse.iee.editor.monitoring.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NewPadWizard extends Wizard implements INewWizard {
+
+	public NewPadWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
