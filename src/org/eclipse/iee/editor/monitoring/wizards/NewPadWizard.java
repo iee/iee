073ee367373ff3fc014278814ed2359ebb791984
khavrenko <khavrenkosergey@gmail.com>
2011-10-31 18:32:59 +0400

    Working on #3

--- src/org/eclipse/iee/editor/monitoring/wizards/NewPadWizard.java
+++ src/org/eclipse/iee/editor/monitoring/wizards/NewPadWizard.java
@@ -1,37 +1,36 @@
 package org.eclipse.iee.editor.monitoring.wizards;
 
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardContainer;
+import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.pde.ui.IFieldData;
+import org.eclipse.pde.ui.templates.ITemplateSection;
+import org.eclipse.pde.ui.templates.NewPluginTemplateWizard;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 
-public class NewPadWizard extends Wizard implements INewWizard {
+public class NewPadWizard extends NewPluginTemplateWizard implements INewWizard {
 
 	private WizardNewProjectCreationPage _pageOne;
-
-	public NewPadWizard() {
-	}
+	protected IFieldData fData;
 
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		super.init(getData());
+        setWindowTitle("New Pad Wizard");
 	}
 
 	@Override
-	public boolean performFinish() {
-		return true;
+	public ITemplateSection[] createTemplateSections() {
+		return null;
+		//return new ITemplateSection[] {new SimpleViewTemplateSection()};
 	}
 	
-	@Override
-	public void addPages() {
-		super.addPages();
-		_pageOne = new WizardNewProjectCreationPage(
-				"IEE NewPad Plug-in Project Wizard");
-		_pageOne.setTitle("IEE NewPad Plug-in Project");
-		_pageOne.setDescription("Creates sample pad project.");
-
-		addPage(_pageOne);
-
-	}
 
 }
