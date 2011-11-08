1a9c67caeb7180a0694aab9f1d8bdb2946dfd5a5
khavrenko <khavrenkosergey@gmail.com>
2011-11-09 02:44:46 +0400

    Working on #3

--- src/org/eclipse/iee/editor/monitoring/templates/PadTemplate.java
+++ src/org/eclipse/iee/editor/monitoring/templates/PadTemplate.java
@@ -24,28 +24,29 @@ import org.eclipse.pde.ui.templates.TemplateOption;
 
 public class PadTemplate extends OptionTemplateSection {
 
-	private static final String KEY_CLASS_NAME = "className";
+	private static final String KEY_PAD_NAME = "padName";
+	// private static final String KEY_ICON_LOCATION = "iconLocation";
 	private String packageName = null;
-	
+
 	public PadTemplate() {
 		super();
 		setPageCount(1);
 		createOptions();
 	}
-	
+
 	private void createOptions() {
-		addOption(KEY_CLASS_NAME, "Class Name ", "NewPad", 0);
+		addOption(KEY_PAD_NAME, "Pad Name ", "NewPad", 0);
 	}
-	
+
 	public void addPages(Wizard wizard) {
-		//WizardPage page = createPage(0, IHelpContextIds.TEMPLATE_INTRO);
+		// WizardPage page = createPage(0, IHelpContextIds.TEMPLATE_INTRO);
 		WizardPage page = createPage(0);
 		page.setTitle("Pad Template");
 		page.setDescription("Creates a new pad");
 		wizard.addPage(page);
 		markPagesAdded();
 	}
-	
+
 	/**
 	 * This is the folder relative to your install url and template directory
 	 * where the templates are stored.
@@ -53,7 +54,7 @@ public class PadTemplate extends OptionTemplateSection {
 	public String getSectionId() {
 		return "padtemplate";
 	}
-	
+
 	protected void initializeFields(IFieldData data) {
 		String id = data.getId();
 		initializeOption(KEY_PACKAGE_NAME, getFormattedPackageName(id));
@@ -64,13 +65,69 @@ public class PadTemplate extends OptionTemplateSection {
 	 * Validate your options here!
 	 */
 	public void validateOptions(TemplateOption changed) {
-		// TODO Auto-generated method stub
 
 	}
 
 	protected void updateModel(IProgressMonitor monitor) throws CoreException {
 		IPluginBase plugin = model.getPluginBase();
 		IPluginModelFactory factory = model.getPluginFactory();
+
+		IPluginExtension menuExtension = createExtension(
+				"org.eclipse.ui.popupMenus", true);
+		IPluginElement sampleViewerContribution = factory
+				.createElement(menuExtension);
+
+		sampleViewerContribution.setAttribute("id",
+				getStringOption(KEY_PACKAGE_NAME) + ".contextSample");
+		sampleViewerContribution.setAttribute("targetID", "#TextEditorContext");
+
+		IPluginElement contextAddAction = factory
+				.createElement(sampleViewerContribution);
+		contextAddAction.setAttribute("id", getStringOption(KEY_PACKAGE_NAME)
+				+ ".actions.Add" + getStringOption(KEY_PAD_NAME)
+				+ "ActionDelegate");
+		contextAddAction.setAttribute("label", "Insert "
+				+ getStringOption(KEY_PAD_NAME));
+		contextAddAction.setAttribute("class",
+				getStringOption(KEY_PACKAGE_NAME) + ".actions.Add"
+						+ getStringOption(KEY_PAD_NAME) + "ActionDelegate");
+		contextAddAction.setAttribute("menubarPath", "additions");
+		// contextAddAction.setAttribute("icon", "");
+		contextAddAction.setAttribute("tooltip", "Insert "
+				+ getStringOption(KEY_PAD_NAME));
+		menuExtension.add(contextAddAction);
+
+		plugin.add(menuExtension);
+
+		IPluginExtension toolbarExtension = createExtension(
+				"org.eclipse.ui.editorActions", true);
+
+		IPluginElement sampleEditorContribution = factory
+				.createElement(menuExtension);
+
+		sampleEditorContribution.setAttribute("id",
+				getStringOption(KEY_PACKAGE_NAME) + ".toolbarSample");
+		sampleEditorContribution.setAttribute("targetID",
+				"org.eclipse.iee.editor.sample.SampleExtendedEditor");
+
+		IPluginElement toolbarAddAction = factory
+				.createElement(sampleEditorContribution);
+		toolbarAddAction.setAttribute("id", getStringOption(KEY_PACKAGE_NAME)
+				+ ".actions.Add" + getStringOption(KEY_PAD_NAME)
+				+ "ActionDelegate");
+		toolbarAddAction.setAttribute("label", "Insert "
+				+ getStringOption(KEY_PAD_NAME));
+		toolbarAddAction.setAttribute("class",
+				getStringOption(KEY_PACKAGE_NAME) + ".actions.Add"
+						+ getStringOption(KEY_PAD_NAME) + "ActionDelegate");
+		toolbarAddAction.setAttribute("toolbarPath", "TextEditor");
+		// contextAddAction.setAttribute("icon", "");
+		toolbarAddAction.setAttribute("tooltip", "Insert "
+				+ getStringOption(KEY_PAD_NAME));
+		toolbarExtension.add(toolbarAddAction);
+
+		plugin.add(toolbarExtension);
+
 	}
 
 	public String getUsedExtensionPoint() {
@@ -94,35 +151,35 @@ public class PadTemplate extends OptionTemplateSection {
 	public String[] getNewFiles() {
 		return new String[0];
 	}
-	
+
 	public IPluginReference[] getDependencies(String schemaVersion) {
 		ArrayList<PluginReference> result = new ArrayList<PluginReference>();
- 
-        result.add(new PluginReference("org.eclipse.core.runtime", null, 0)); 
-        result.add(new PluginReference("org.eclipse.ui", null, 0));
-        result.add(new PluginReference("org.eclipse.iee.editor", "1.0.0", 0));
-        result.add(new PluginReference("org.eclipse.ui.editors", "3.7.0", 0));
 
-		return (IPluginReference[]) result.toArray(
-				new IPluginReference[result.size()]);
+		result.add(new PluginReference("org.eclipse.core.runtime", null, 0));
+		result.add(new PluginReference("org.eclipse.ui", null, 0));
+		result.add(new PluginReference("org.eclipse.iee.editor", "1.0.0", 0));
+		result.add(new PluginReference("org.eclipse.ui.editors", "3.7.0", 0));
+
+		return (IPluginReference[]) result.toArray(new IPluginReference[result
+				.size()]);
 	}
-	
+
 	public boolean isDependentOnParentWizard() {
 		return true;
 	}
-	
+
 	public int getNumberOfWorkUnits() {
 		return super.getNumberOfWorkUnits() + 1;
 	}
-	
+
 	public String getStringOption(String name) {
 		if (name.equals(KEY_PACKAGE_NAME)) {
 			return packageName;
 		}
 		return super.getStringOption(name);
 	}
-	
-	protected String getFormattedPackageName(String id){
+
+	protected String getFormattedPackageName(String id) {
 		StringBuffer buffer = new StringBuffer();
 		for (int i = 0; i < id.length(); i++) {
 			char ch = id.charAt(i);
@@ -136,7 +193,6 @@ public class PadTemplate extends OptionTemplateSection {
 		}
 		return buffer.toString().toLowerCase(Locale.ENGLISH);
 	}
-	
 
 	public String getDescription() {
 		return "Creates a new pad";
