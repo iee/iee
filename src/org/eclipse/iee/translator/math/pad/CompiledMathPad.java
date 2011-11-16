462c41b736f6c7dbaf5c9d508bb1b61c2da34c20
khavrenko <khavrenkosergey@gmail.com>
2011-11-16 22:19:21 +0400

    Add expressions drawing (after CR)

--- src/org/eclipse/iee/translator/math/pad/CompiledMathPad.java
+++ src/org/eclipse/iee/translator/math/pad/CompiledMathPad.java
@@ -1,21 +1,31 @@
 package org.eclipse.iee.translator.math.pad;
 
 import java.awt.Color;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 import org.eclipse.iee.editor.core.pad.Pad;
 import org.eclipse.iee.translator.math.Activator;
 import org.eclipse.iee.translator.math.FileStorage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
-import org.scilab.forge.jlatexmath.TeXIcon;
 
 public class CompiledMathPad extends Pad implements Serializable {
 
@@ -25,6 +35,7 @@ public class CompiledMathPad extends Pad implements Serializable {
 	private String fLatexContent;
 	private String fJavaContent;
 	private String fImagePath;
+	private boolean fIsTextVisible;
 
 	public CompiledMathPad() {
 		super();
@@ -32,9 +43,10 @@ public class CompiledMathPad extends Pad implements Serializable {
 				+ ".jpg";
 		setType("CompiledMath");
 		fText = "";
+		fIsTextVisible = true;
 		fLatexContent = "";
 		fJavaContent = "";
-		//save();
+		// save();
 	}
 
 	@Override
@@ -43,50 +55,110 @@ public class CompiledMathPad extends Pad implements Serializable {
 		layout.marginHeight = 5;
 		layout.marginWidth = 5;
 		parent.setLayout(layout);
-		final StyledText styledText = new StyledText(parent, SWT.NONE);
+		SashForm sashForm = new SashForm(parent, SWT.HORIZONTAL);
+		sashForm.setLayout(new FillLayout());
+		final StyledText styledText = new StyledText(sashForm, SWT.NONE);
 		styledText.setText(fText);
+		styledText.setVisible(fIsTextVisible);
 
-//		final Label label = new Label(parent, SWT.NONE | SWT.RESIZE);
-//		label.setVisible(false);
+		final Label label = new Label(sashForm, SWT.NONE | SWT.RESIZE);
+		if (!fImagePath.isEmpty()) {
+			File imageFile = new File(fImagePath);
+			Image image = null;
+			if (imageFile.exists()) {
+				try {
+					image = new Image(parent.getDisplay(), fImagePath);
+				} catch (Exception exception) {
+					exception.printStackTrace();
+				}
+				label.setImage(image);
+				parent.pack();
+			}
+		}
+		label.setToolTipText("Use DoubleClick for edit");
+		label.setVisible(!fIsTextVisible);
+
+		parent.pack();
 
 		// Listeners
 		styledText.addModifyListener(new ModifyListener() {
 			@Override
 			public void modifyText(ModifyEvent e) {
-				System.out.println("before translation to Java");
-				try {
-					String resultJava = Activator.getMole().translateMath(
-							styledText.getText());
-					fJavaContent = resultJava.trim();
-					System.out.println(fJavaContent);
-					if (fJavaContent.matches(";")) {
+				if (!styledText.getText().contains("\n")) {
+					System.out.println("before translation to Java");
+					try {
+						fText = styledText.getText();
+						String resultJava = Activator.getMole().translateMath(
+								styledText.getText());
+						fJavaContent = resultJava.trim();
+						System.out.println(fJavaContent);
+						if (fJavaContent.matches(";")) {
+							fJavaContent = "";
+							getContainer().writeAtContainerRegionTail(
+									fJavaContent);
+						} else
+							getContainer().writeAtContainerRegionTail(
+									fJavaContent);
+					} catch (Exception e1) {
 						fJavaContent = "";
 						getContainer().writeAtContainerRegionTail(fJavaContent);
-					} else
-						getContainer().writeAtContainerRegionTail(fJavaContent);
-				} catch (Exception e1) {
-					fJavaContent = "";
-					getContainer().writeAtContainerRegionTail(fJavaContent);
-					// e1.printStackTrace();
-				}
+						// e1.printStackTrace();
+					}
 
-				System.out.println("before translation to TeX");
-				try {
-					String resultTex = Activator.getMolex().translateMath(
-							styledText.getText());
-					String hidden = resultTex.trim();
-					fLatexContent = hidden;
-					System.out.println(fLatexContent);
-					TeXFormula texFormula = new TeXFormula(fLatexContent);
-					texFormula.createJPEG(TeXConstants.STYLE_DISPLAY, 40,
-							fImagePath, Color.white, Color.black);
-				} catch (Exception e1) {
-					fLatexContent = "";
-					// e1.printStackTrace();
+					System.out.println("before translation to TeX");
+					try {
+						String resultTex = Activator.getMolex().translateMath(
+								styledText.getText());
+						String hidden = resultTex.trim();
+						fLatexContent = hidden;
+						System.out.println(fLatexContent);
+						TeXFormula texFormula = new TeXFormula(fLatexContent);
+						texFormula.createJPEG(TeXConstants.STYLE_DISPLAY, 30,
+								fImagePath, Color.white, Color.black);
+					} catch (Exception e1) {
+						fLatexContent = "";
+						// e1.printStackTrace();
+					}
+				} else {
+					styledText.setVisible(false);
+					fIsTextVisible = false;
+					Image image = null;
+					try {
+						image = new Image(parent.getDisplay(), fImagePath);
+					} catch (Exception exception) {
+						exception.printStackTrace();
+					}
+					label.setImage(image);
+					label.setSize(image.getBounds().width,
+							image.getBounds().height);
+					label.setVisible(true);
+					parent.pack();
 				}
-
 			}
 
+		});
+
+		label.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseUp(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseDown(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				if (!fIsTextVisible) {
+					fIsTextVisible = true;
+					styledText.setText(fText);
+					styledText.setVisible(fIsTextVisible);
+					label.setVisible(!fIsTextVisible);
+					parent.pack();
+					save();
+				}
+			}
 		});
 
 		MouseEventManager mouseManager = new MouseEventManager(parent);
@@ -101,7 +173,8 @@ public class CompiledMathPad extends Pad implements Serializable {
 		fImagePath = fFileStorage.getDirectoryPath() + containerID + ".jpg";
 		fLatexContent = "";
 		fJavaContent = "";
-		//save();
+		fIsTextVisible = true;
+		save();
 	}
 
 	public static void setStorage(FileStorage fStorage) {
@@ -114,6 +187,30 @@ public class CompiledMathPad extends Pad implements Serializable {
 		newPad.fText = this.fText;
 		newPad.fLatexContent = this.fLatexContent;
 		newPad.fJavaContent = this.fJavaContent;
+		newPad.fIsTextVisible = this.fIsTextVisible;
+		
+		File inputFile = new File(this.fImagePath);
+		File outputFile = new File(newPad.fImagePath);
+
+		try {
+			BufferedInputStream bis = new BufferedInputStream(
+					new FileInputStream(inputFile), 4096);
+			BufferedOutputStream bos = new BufferedOutputStream(
+					new FileOutputStream(outputFile), 4096);
+			int buffer;
+			while ((buffer = bis.read()) != -1) {
+				bos.write(buffer);
+			}
+			bos.close();
+			bis.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return newPad;
 	}
 
