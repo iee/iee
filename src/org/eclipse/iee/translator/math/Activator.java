d24e03f470565b7e29245f6d2c99517d3f58f04b
khavrenko <khavrenkosergey@gmail.com>
2011-11-16 15:55:10 +0400

    Add TeX translator support

--- src/org/eclipse/iee/translator/math/Activator.java
+++ src/org/eclipse/iee/translator/math/Activator.java
@@ -1,6 +1,7 @@
 package org.eclipse.iee.translator.math;
 
 import org.eclipse.iee.translator.jmole.math.generator.Mole;
+import org.eclipse.iee.translator.molex.math.generator.TexMole;
 import org.eclipse.iee.translator.math.pad.CompiledMathPad;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -18,6 +19,7 @@ public class Activator extends AbstractUIPlugin implements IStartup {
 	private static Activator plugin;
 
 	private static Mole mole;
+	private static TexMole molex;
 	
 	/**
 	 * The constructor
@@ -25,11 +27,21 @@ public class Activator extends AbstractUIPlugin implements IStartup {
 	public Activator() {
 		CompiledMathPad.setStorage(new FileStorage("D:\\CompiledMathPads\\"));
 		
-		this.mole = Mole.create();
+		//mole = Mole.create();
+		//molex = TexMole.create();
+		//System.out.println("Activated");
 	}
 	
 	public static Mole getMole() {
+		molex = null;
+		mole = Mole.create();
 		return mole;
+	}
+	
+	public static TexMole getMolex() {
+		mole = null;
+		molex = TexMole.create();
+		return molex;
 	}
 
 	/*
--- src/org/eclipse/iee/translator/math/pad/CompiledMathPad.java
+++ src/org/eclipse/iee/translator/math/pad/CompiledMathPad.java
@@ -1,5 +1,6 @@
 package org.eclipse.iee.translator.math.pad;
 
+import java.awt.Color;
 import java.io.Serializable;
 
 import org.eclipse.iee.editor.core.pad.Pad;
@@ -11,17 +12,29 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.scilab.forge.jlatexmath.TeXConstants;
+import org.scilab.forge.jlatexmath.TeXFormula;
+import org.scilab.forge.jlatexmath.TeXIcon;
 
 public class CompiledMathPad extends Pad implements Serializable {
 
 	private transient static FileStorage fFileStorage;
 
 	private String fText;
+	private String fLatexContent;
+	private String fJavaContent;
+	private String fImagePath;
 
 	public CompiledMathPad() {
 		super();
+		fImagePath = fFileStorage.getDirectoryPath() + this.getContainerID()
+				+ ".jpg";
 		setType("CompiledMath");
 		fText = "";
+		fLatexContent = "";
+		fJavaContent = "";
+		//save();
 	}
 
 	@Override
@@ -32,28 +45,50 @@ public class CompiledMathPad extends Pad implements Serializable {
 		parent.setLayout(layout);
 		final StyledText styledText = new StyledText(parent, SWT.NONE);
 		styledText.setText(fText);
+
+//		final Label label = new Label(parent, SWT.NONE | SWT.RESIZE);
+//		label.setVisible(false);
+
+		// Listeners
 		styledText.addModifyListener(new ModifyListener() {
 			@Override
 			public void modifyText(ModifyEvent e) {
-				// System.out.println(styledText.getText());
-
-				System.out.println("before translation");
+				System.out.println("before translation to Java");
 				try {
-					String result = Activator.getMole().translateMath(
+					String resultJava = Activator.getMole().translateMath(
 							styledText.getText());
-					String hidden = result.trim();
-					if (hidden.matches(";"))
-						getContainer().writeAtContainerRegionTail("");
-					else
-						getContainer().writeAtContainerRegionTail(hidden);
+					fJavaContent = resultJava.trim();
+					System.out.println(fJavaContent);
+					if (fJavaContent.matches(";")) {
+						fJavaContent = "";
+						getContainer().writeAtContainerRegionTail(fJavaContent);
+					} else
+						getContainer().writeAtContainerRegionTail(fJavaContent);
 				} catch (Exception e1) {
-					getContainer().writeAtContainerRegionTail("");
+					fJavaContent = "";
+					getContainer().writeAtContainerRegionTail(fJavaContent);
+					// e1.printStackTrace();
+				}
+
+				System.out.println("before translation to TeX");
+				try {
+					String resultTex = Activator.getMolex().translateMath(
+							styledText.getText());
+					String hidden = resultTex.trim();
+					fLatexContent = hidden;
+					System.out.println(fLatexContent);
+					TeXFormula texFormula = new TeXFormula(fLatexContent);
+					texFormula.createJPEG(TeXConstants.STYLE_DISPLAY, 40,
+							fImagePath, Color.white, Color.black);
+				} catch (Exception e1) {
+					fLatexContent = "";
 					// e1.printStackTrace();
 				}
 
 			}
 
 		});
+
 		MouseEventManager mouseManager = new MouseEventManager(parent);
 		parent.addMouseTrackListener(mouseManager);
 		parent.addMouseMoveListener(mouseManager);
@@ -63,6 +98,10 @@ public class CompiledMathPad extends Pad implements Serializable {
 
 	protected CompiledMathPad(String containerID) {
 		super(containerID);
+		fImagePath = fFileStorage.getDirectoryPath() + containerID + ".jpg";
+		fLatexContent = "";
+		fJavaContent = "";
+		//save();
 	}
 
 	public static void setStorage(FileStorage fStorage) {
@@ -73,6 +112,8 @@ public class CompiledMathPad extends Pad implements Serializable {
 	public Pad copy() {
 		CompiledMathPad newPad = new CompiledMathPad();
 		newPad.fText = this.fText;
+		newPad.fLatexContent = this.fLatexContent;
+		newPad.fJavaContent = this.fJavaContent;
 		return newPad;
 	}
 
