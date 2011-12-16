b4fb4214adc4ba54f746cb2086ea6107605c6a0e
khavrenko <khavrenkosergey@gmail.com>
2011-12-16 14:34:43 +0400

    add classes

---
+++ src/org/eclipse/iee/sample/matrix/actions/AddMatrixActionDelegate.java
@@ -0,0 +1,9 @@
+package org.eclipse.iee.sample.matrix.actions;
+
+public class AddMatrixActionDelegate {
+
+	public AddMatrixActionDelegate() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/org/eclipse/iee/sample/matrix/pad/MatrixPad.java
@@ -0,0 +1,38 @@
+package org.eclipse.iee.sample.matrix.pad;
+
+import org.eclipse.iee.editor.core.pad.Pad;
+import org.eclipse.swt.widgets.Composite;
+
+public class MatrixPad extends Pad{
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Pad copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unsave() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onContainerAttached() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
