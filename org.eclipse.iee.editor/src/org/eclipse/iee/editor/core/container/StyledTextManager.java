51426a3ba03d090612530ff7937864449985ae94
Artem <artem.cpt@gmail.com>
2012-09-07 19:00:35 +0400

    Text color fix

--- org.eclipse.iee.editor/src/org/eclipse/iee/editor/core/container/StyledTextManager.java
+++ org.eclipse.iee.editor/src/org/eclipse/iee/editor/core/container/StyledTextManager.java
@@ -94,6 +94,12 @@ class StyledTextManager {
 					
 				case UPDATED_TWICE:
 				case UPDATED_MORE_THAN_TWICE:
+					
+					// Еще одно исправление непонятного бага.
+					// При последнем вызове applyTextPresentation() почему-то не передаются стили jdt,
+					// поэтому выставляем те стили, которые уже имеются
+					textPresentation.mergeStyleRanges(fStyledText.getStyleRanges());
+					
 					injectStylesToTextPresentation(textPresentation, getContainersStyleRanges());
 					
 					fStylesIterationStatus = IterationStatus.UPDATED_MORE_THAN_TWICE;
@@ -187,7 +193,7 @@ class StyledTextManager {
 			}
 		}
 	}
-	
+		
 	protected void injectStylesToTextPresentation(TextPresentation tp, StyleRange[] containersStyleRanges) {
 		
 		List<StyleRange> allowedStyleRanges = new ArrayList<StyleRange>();
@@ -204,7 +210,7 @@ class StyledTextManager {
 				}
 			}
 			
-			if (!overlap) {		
+			if (!overlap) {
 				allowedStyleRanges.add(styleRange);
 			}
 		}
