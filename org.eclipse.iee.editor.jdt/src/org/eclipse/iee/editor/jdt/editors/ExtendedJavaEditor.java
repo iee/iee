469a4a7e62a8dfdbc30784fbda882194e22f5fe5
khavrenko <khavrenkosergey@gmail.com>
2012-10-05 12:21:41 +0400

    fix pads loading

--- org.eclipse.iee.editor.jdt/src/org/eclipse/iee/editor/jdt/editors/ExtendedJavaEditor.java
+++ org.eclipse.iee.editor.jdt/src/org/eclipse/iee/editor/jdt/editors/ExtendedJavaEditor.java
@@ -28,45 +28,44 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 
 @SuppressWarnings("restriction")
-public class ExtendedJavaEditor extends CompilationUnitEditor implements IPadEditor {
+public class ExtendedJavaEditor extends CompilationUnitEditor implements
+		IPadEditor {
 
-	private static final Logger logger = Logger.getLogger(ExtendedJavaEditor.class);
-	
+	private static final Logger logger = Logger
+			.getLogger(ExtendedJavaEditor.class);
+
 	private ContainerManager fContainerManager;
 	private IContainerManagerListener fContainerManagerListener;
 
-	private final PadManager fPadManager =
-		IeeEditorPlugin.getDefault().getPadManager();
+	private final PadManager fPadManager = IeeEditorPlugin.getDefault()
+			.getPadManager();
 
 	public ExtendedJavaEditor() {
 		super();
 	}
-	
+
 	public void createPartControl(Composite parent) {
 		super.createPartControl(parent);
-		
+
 		logger.debug("Create ExtendedJavaEditor");
-		
+
 		initIeeEditorCore();
-		
+
 		loadEditorPads();
 	};
-	
+
 	public void initIeeEditorCore() {
 		IDocument document = getSourceViewer().getDocument();
-		
+
 		ContainerManagerConfig config = new ContainerManagerConfig();
 
-		//config.EMBEDDED_REGION_BEGIN = "/*<";
-		//config.EMBEDDED_REGION_END = ">*/";
-		//config.INNER_TEXT_BEGIN = "*/";
-		//config.INNER_TEXT_END = "/*";
-		
-		fContainerManager = new ContainerManager(
-			config,
-			document,
-			getSourceViewer(),
-			getSourceViewer().getTextWidget());
+		// config.EMBEDDED_REGION_BEGIN = "/*<";
+		// config.EMBEDDED_REGION_END = ">*/";
+		// config.INNER_TEXT_BEGIN = "*/";
+		// config.INNER_TEXT_END = "/*";
+
+		fContainerManager = new ContainerManager(config, document,
+				getSourceViewer(), getSourceViewer().getTextWidget());
 
 		fContainerManagerListener = new IContainerManagerListener() {
 			@Override
@@ -87,14 +86,15 @@ public class ExtendedJavaEditor extends CompilationUnitEditor implements IPadEdi
 			}
 
 			@Override
-			public void containerLostSelection(ContainerEvent event) {				
+			public void containerLostSelection(ContainerEvent event) {
 			}
 
 			@Override
 			public void containerActivated(ContainerEvent event) {
 			}
 		};
-		fContainerManager.addContainerManagerListener(fContainerManagerListener);
+		fContainerManager
+				.addContainerManagerListener(fContainerManagerListener);
 
 		fPadManager.registerContainerManager(fContainerManager);
 
@@ -107,77 +107,81 @@ public class ExtendedJavaEditor extends CompilationUnitEditor implements IPadEdi
 			String text = document.get();
 			document.replace(0, text.length(), "");
 			document.set(text);
-			
+
 		} catch (BadLocationException e) {
 			logger.error(e.getMessage());
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	private void loadEditorPads() {
 		logger.debug("loadEditorPads");
-		
-		IEditorPart editor = (IEditorPart)this;
-		IFileEditorInput input = (IFileEditorInput)editor.getEditorInput();
-	    IFile file = input.getFile();
-	    IProject project = file.getProject();
-	    
-	    IPath rawLocation = project.getRawLocation();
-	    
-	    String storagePath = "";
-	    
-	    if (rawLocation != null)
-	    {
-	    	storagePath = rawLocation.makeAbsolute().toString() + "/pads/";
-	    }
-	    else
-	    {
-	    	IWorkspace workspace = ResourcesPlugin.getWorkspace();  
-	    	IPath workspaceDirectory = workspace.getRoot().getLocation();
-	    	storagePath = workspaceDirectory.toString() + project.getFullPath().makeAbsolute().toString() + "/pads/";
-	    }
-	    
-	    logger.debug("storagePath = " + storagePath);
-	    
-	    String[] containersIDs = fContainerManager.getContainerIDs();
-	    
-	    if (containersIDs.length > 0)
-	    {
-		    File formulaStorage = new File(storagePath + "formula/");
-		    String[] formulaSerializedPads = formulaStorage.list();
-		    
-		    int i,j;
-		    for (i = 0; i < containersIDs.length; i++)
-		    	for(j = 0; j < formulaSerializedPads.length; j++)
-		    	{
-		    		if (containersIDs[i].matches(formulaSerializedPads[j]))
-		    		{
-		    			FormulaFileStorage.getInstance(storagePath + "formula/").loadFromFile(containersIDs[i]);
-		    		}
-		    	}
-		    
-		    File imageStorage = new File(storagePath + "image/");
-		    String[] imageSerializedPads = imageStorage.list();
-		    
-		    for (i = 0; i < containersIDs.length; i++)
-		    	for(j = 0; j < imageSerializedPads.length; j++)
-		    	{
-		    		if (containersIDs[i].matches(imageSerializedPads[j]))
-		    		{
-		    			ImageFileStorage.getInstance(storagePath + "image/").loadFromFile(containersIDs[i]);
-		    		}
-		    	}
-	    }
+
+		IEditorPart editor = (IEditorPart) this;
+		IFileEditorInput input = (IFileEditorInput) editor.getEditorInput();
+		IFile file = input.getFile();
+		IProject project = file.getProject();
+
+		IPath rawLocation = project.getRawLocation();
+
+		String storagePath = "";
+
+		if (rawLocation != null) {
+			storagePath = rawLocation.makeAbsolute().toString() + "/pads/";
+		} else {
+			IWorkspace workspace = ResourcesPlugin.getWorkspace();
+			IPath workspaceDirectory = workspace.getRoot().getLocation();
+			storagePath = workspaceDirectory.toString()
+					+ project.getFullPath().makeAbsolute().toString()
+					+ "/pads/";
+		}
+
+		logger.debug("storagePath = " + storagePath);
+
+		String[] containersIDs = fContainerManager.getContainerIDs();
+
+		if (containersIDs.length > 0) {
+			File formulaStorage = new File(storagePath + "formula/");
+			String[] formulaSerializedPads = formulaStorage.list();
+
+			int i, j;
+
+			if (formulaSerializedPads != null) {
+				for (i = 0; i < containersIDs.length; i++)
+					for (j = 0; j < formulaSerializedPads.length; j++) {
+						if (containersIDs[i].matches(formulaSerializedPads[j])) {
+							FormulaFileStorage.getInstance(
+									storagePath + "formula/").loadFromFile(
+									containersIDs[i]);
+						}
+					}
+			}
+
+			File imageStorage = new File(storagePath + "image/");
+			String[] imageSerializedPads = imageStorage.list();
+
+			if (imageSerializedPads != null) {
+				for (i = 0; i < containersIDs.length; i++)
+					for (j = 0; j < imageSerializedPads.length; j++) {
+						if (containersIDs[i].matches(imageSerializedPads[j])) {
+							ImageFileStorage
+									.getInstance(storagePath + "image/")
+									.loadFromFile(containersIDs[i]);
+						}
+					}
+			}
+			
+		}
 	}
 
-	
 	@Override
 	public void dispose() {
 		logger.debug("dispose() called");
-		
+
 		fPadManager.removeContainerManager(fContainerManager);
-		fContainerManager.removeContainerManagerListener(fContainerManagerListener);
+		fContainerManager
+				.removeContainerManagerListener(fContainerManagerListener);
 		fContainerManager.dispose();
 		fContainerManager = null;
 
