bd28c97c737b94e320772358f1c2b9e23c9deee9
khavrenko <khavrenkosergey@gmail.com>
2013-04-08 19:43:52 +0400

    start working on pdf export

---
+++ org.eclipse.iee.editor.monitoring/src/org/eclipse/iee/editor/monitoring/handlers/PdfExportHandler.java
@@ -0,0 +1,70 @@
+package org.eclipse.iee.editor.monitoring.handlers;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.iee.editor.IPadEditor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class PdfExportHandler implements IHandler {
+
+	private static final Logger logger = Logger
+			.getLogger(PdfExportHandler.class);
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		logger.debug("Export to pdf");
+
+		Shell shell = HandlerUtil.getActiveWorkbenchWindow(event).getShell();
+		IWorkbenchPage page = HandlerUtil.getActiveWorkbenchWindow(event)
+				.getActivePage();
+		IEditorPart targetEditor = page.getActiveEditor();
+
+		IPadEditor fPadEditor;
+		try {
+			fPadEditor = (IPadEditor) targetEditor;
+		} catch (ClassCastException e) {
+			fPadEditor = null;
+		}
+
+		if (fPadEditor == null) {
+			MessageDialog.openError(shell, "Invalid editor", "Invalid editor");
+			return null;
+		}
+
+		//TODO: add export
+
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+	}
+
+}
+
