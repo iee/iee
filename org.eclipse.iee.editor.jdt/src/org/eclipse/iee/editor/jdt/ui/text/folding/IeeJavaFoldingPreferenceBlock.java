49a6d1ec31fc2b904bae26acdbb75d69bfb2a255
khavrenko <khavrenkosergey@gmail.com>
2013-06-10 20:36:21 +0400

    start working on folding extension

---
+++ org.eclipse.iee.editor.jdt/src/org/eclipse/iee/editor/jdt/ui/text/folding/IeeJavaFoldingPreferenceBlock.java
@@ -0,0 +1,40 @@
+package org.eclipse.iee.editor.jdt.ui.text.folding;
+
+import org.eclipse.jdt.ui.text.folding.IJavaFoldingPreferenceBlock;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+public class IeeJavaFoldingPreferenceBlock implements
+		IJavaFoldingPreferenceBlock {
+
+	@Override
+	public Control createControl(Composite parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void performOk() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void performDefaults() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ org.eclipse.iee.editor.jdt/src/org/eclipse/iee/editor/jdt/ui/text/folding/IeeJavaFoldingStructureProvider.java
@@ -0,0 +1,5 @@
+package org.eclipse.iee.editor.jdt.ui.text.folding;
+
+public class IeeJavaFoldingStructureProvider {
+
+}
