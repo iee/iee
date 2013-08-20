90d890a7574094b6299144702c44b462ce8c79f4
qnox <anton.efimchuk@gmail.com>
2013-08-20 22:48:31 +0400

    Changed FileMessenger to use standard Java 7.0 filesystem watcher api

--- org.eclipse.iee.editor/src/org/eclipse/iee/editor/core/utils/runtime/file/FileMessager.java
+++ org.eclipse.iee.editor/src/org/eclipse/iee/editor/core/utils/runtime/file/FileMessager.java
@@ -1,7 +1,22 @@
 package org.eclipse.iee.editor.core.utils.runtime.file;
 
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -14,6 +29,10 @@ import org.apache.commons.io.monitor.FileAlterationObserver;
 import org.apache.log4j.Logger;
 import org.eclipse.core.commands.common.EventManager;
 import org.eclipse.core.runtime.Assert;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+
+import com.google.common.base.Throwables;
 
 public class FileMessager extends EventManager {
 
@@ -23,6 +42,10 @@ public class FileMessager extends EventManager {
 
 	private static FileMessager fFileMessager = new FileMessager();
 
+	private Thread watcherThread;
+
+	private WatchService watcher;
+	
 	public static FileMessager getInstance() {
 		return fFileMessager;
 	}
@@ -33,6 +56,30 @@ public class FileMessager extends EventManager {
 
 	protected FileMessager() {
 		fObservers = new HashMap<String, FileAlterationObserver>();
+		try {
+			watcher = FileSystems.getDefault().newWatchService();
+		} catch (IOException e) {
+			Throwables.propagate(e);
+		}
+		watcherThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (;;) {
+					WatchKey key;
+				    try {
+				        key = watcher.take();
+				    } catch (InterruptedException x) {
+				        return;
+				    }
+					checkRuntimeValues();
+					boolean valid = key.reset();
+			        if (!valid) {
+			            break;
+			        }
+				}
+			}
+		});
+		watcherThread.start();
 	}
 
 	public void clear() {
@@ -45,9 +92,15 @@ public class FileMessager extends EventManager {
 		addListenerObject(listener);
 
 		if (!fObservers.containsKey(path)) {
-
+			String runtime = path + getRuntimeDirectoryName();
+			try {
+				Paths.get(runtime).register(watcher, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE);
+			} catch (IOException e) {
+				Throwables.propagate(e);
+			}
+			
 			FileAlterationObserver observer = new FileAlterationObserver(
-					new File(path + getRuntimeDirectoryName()));
+					new File(runtime));
 
 			observer.addListener(new FileAlterationListener() {
 
