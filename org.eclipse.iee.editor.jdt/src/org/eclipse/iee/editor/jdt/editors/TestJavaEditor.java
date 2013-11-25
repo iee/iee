79ee45c4f7368a90e5edad535c0eb1cbfbbe625a
qnox <anton.efimchuk@gmail.com>
2013-11-25 04:34:59 +0400

    added test editor for test activities

---
+++ org.eclipse.iee.editor.jdt/src/org/eclipse/iee/editor/jdt/editors/TestJavaEditor.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package org.eclipse.iee.editor.jdt.editors;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.part.MultiPageEditorPart;
+
+public class TestJavaEditor extends MultiPageEditorPart {
+
+	
+	
+	private CompilationUnitEditor editor;
+
+	@Override
+	protected void createPages() {
+		
+		try {
+			int textIndex = addPage(new TextEditor(), getEditorInput());
+			setPageText(textIndex, "Text");
+			editor = new CompilationUnitEditor();
+			int javaIndex = addPage(editor, getEditorInput());
+			setPageText(javaIndex, "Java");
+			int ieeIndex = addPage(new ExtendedJavaEditor(), getEditorInput());
+			setPageText(ieeIndex, "IEE");
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		editor.doSave(monitor);
+	}
+
+	@Override
+	public void doSaveAs() {
+		editor.doSaveAs();
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		return editor.isSaveAsAllowed();
+	}
+
+}
